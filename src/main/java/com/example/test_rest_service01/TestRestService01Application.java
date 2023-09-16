bf72a8f532a885cfb306677d774bbb9d106051de
ParubovDaniil <ParubovDaniil@mail.ru>
2023-09-16 17:01:04 +0500

    Initial Commit

---
+++ src/main/java/com/example/test_rest_service01/TestRestService01Application.java
@@ -0,0 +1,13 @@
+package com.example.test_rest_service01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestRestService01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestRestService01Application.class, args);
+    }
+
+}
